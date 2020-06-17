dc49cbb509dab34534aa63e65f2414b65f973e2d
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-06-17 06:34:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here
+class Breakfast{
+  constructor(food,drink){
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+let Breakfast1 = new Breakfast("egg","milk");
+
+
+class Lunch{
+  constructor(salad,soup,drink,){
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+let newlunch = new Lunch("veg salad","adas","soda");
+
+
+class Dinner{
+  constructor(food,drink){
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+let dinner1 = new Dinner("steak","cola");
